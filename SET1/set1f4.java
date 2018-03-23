e78644cfce5be88969fb7dde0af12b435eddbd5c
pratheeksha97 <36357682+pratheeksha97@users.noreply.github.com>
2018-03-23 17:30:17 +0530

    Create set1f4.java

---
+++ SET1/set1f4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Char
+{
+	public static void main(String[ ] arg)
+	{
+	 char c = '*';
+	if(( c >= 'a' && c <= 'z') || ( c <= 'A' && c >= 'Z'))
+	{
+	System.out.println("the character is a Alphabet");
+	}
+	else
+	{
+	System.out.println("The character is not Alphabet");
+	}
+	
+}
+}
